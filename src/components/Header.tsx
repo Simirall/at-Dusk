1f07fa61dd4271b33bd9ea8f7dc1909deb27a26a
sym-dev <Simiralin@gmail.com>
2021-10-14 21:46:36 +0900

    ログアウトをヘッダーに退避

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,13 @@ import { Flex, Text } from "@chakra-ui/react";
 import { Link as RouterLink } from "react-router-dom";
 import { User } from "misskey-js/built/entities";
 import { ColorModeSwitcher } from "../components/ColorModeSwitcher";
+import { useHistory } from "react-router-dom";
+import { useLoginContext } from "../utils/LoginContext";
 
 export const Header: React.VFC = () => {
   const user = JSON.parse(localStorage.getItem("user") as string) as User;
+  const history = useHistory();
+  const { updateLogin } = useLoginContext();
 
   return (
     <Flex
@@ -21,6 +25,17 @@ export const Header: React.VFC = () => {
       <Flex alignItems="center">
         <Text as={RouterLink} to={`/user/${user.username}`} color="blue.200">
           {user.username}
+        </Text>
+        <Text
+          cursor="pointer"
+          color="teal.500"
+          onClick={() => {
+            localStorage.clear();
+            updateLogin(false);
+            history.push("/login");
+          }}
+        >
+          Logout
         </Text>
         <ColorModeSwitcher boxShadow="base" />
       </Flex>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,6 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
-import { Box, Link } from "@chakra-ui/react";
-import { useLoginContext } from "../utils/LoginContext";
+import { Box } from "@chakra-ui/react";
 
 export const Home: React.VFC = () => {
-  const history = useHistory();
-  const { updateLogin } = useLoginContext();
-  return (
-    <Box textAlign="center" fontSize="xl">
-      <Link
-        color="teal.500"
-        fontSize="2xl"
-        onClick={() => {
-          localStorage.clear();
-          updateLogin(false);
-          history.push("/login");
-        }}
-      >
-        Logout
-      </Link>
-    </Box>
-  );
+  return <Box></Box>;
 };
