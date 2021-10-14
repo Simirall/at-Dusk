4e39202dbd44874a56ded406495bc4fdd709e939
sym-dev <37952374+sym-dev@users.noreply.github.com>
2021-10-14 20:11:59 +0900

    tokenをストアに保持するように

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,5 +1,14 @@
 import React from "react";
 import { Redirect } from "react-router-dom";
+import { useLoginContext } from "../utils/LoginContext";
 
-export const Auth: React.FC = ({ children }) =>
-  localStorage.getItem("login") ? <>{children}</> : <Redirect to={"/login"} />;
+type Props = {
+  children: React.ReactNode;
+};
+
+export const Auth: React.VFC<Props> = ({ children }) => {
+  const { updateToken } = useLoginContext();
+  const isLogin = localStorage.getItem("login");
+  if (isLogin) updateToken(localStorage.getItem("UserToken") as string);
+  return isLogin ? <>{children}</> : <Redirect to={"/login"} />;
+};
