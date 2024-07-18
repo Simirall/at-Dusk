4e51a4ba3c598c0a9cb8ae8063bb36535ae385c4
Simirall <37952374+Simirall@users.noreply.github.com>
2024-07-19 00:23:50 +0900

    add tanstack-router

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,9 @@
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  server: {
-    host: "127.0.0.1",
-    open: true,
-    watch: {
-      usePolling: true,
-    },
-  },
+  plugins: [TanStackRouterVite(), react(), tsconfigPaths()],
 });
