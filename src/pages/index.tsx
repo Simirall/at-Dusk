0d7729832885630f0762be8f9bbbae07630ffff9
Simirall <37952374+Simirall@users.noreply.github.com>
2024-07-22 19:17:12 +0900

    lazy route完全に理解した

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,4 @@
 import { createFileRoute, redirect } from "@tanstack/react-router";
-import {
-  Avatar,
-  Button,
-  Container,
-  HStack,
-  Text,
-  VStack,
-} from "@yamada-ui/react";
-
-import { useTimeLine } from "@/apis/websocket/timeline";
-import { useTimeLineStore } from "@/store/timeline";
 
 export const Route = createFileRoute("/")({
   beforeLoad: ({ context }) => {
@@ -17,33 +6,4 @@ export const Route = createFileRoute("/")({
       throw redirect({ to: "/login" });
     }
   },
-  component: Index,
 });
-
-function Index() {
-  useTimeLine();
-  const { notes } = useTimeLineStore();
-
-  return (
-    <Container>
-      {notes.map((n) => (
-        <VStack key={n.id}>
-          <HStack align="start">
-            <Avatar src={n.user.avatarUrl ?? undefined} />
-            <VStack>
-              <Text>{`${n.user.name} @${n.user.username}${n.user.host ? `@${n.user.host}` : ""}`}</Text>
-              <Text
-                whiteSpace="pre-wrap"
-                wordBreak="keep-all"
-                overflowWrap="anywhere"
-              >
-                {n.text}
-              </Text>
-            </VStack>
-          </HStack>
-        </VStack>
-      ))}
-      {notes.length > 0 && <Button>もっとみる</Button>}
-    </Container>
-  );
-}
--- src/pages/login/_layout/getToken.tsx
+++ src/pages/login/_layout/getToken.tsx
@@ -1,11 +1,5 @@
-import { createFileRoute, useNavigate } from "@tanstack/react-router";
-import { useEffect } from "react";
+import { createFileRoute } from "@tanstack/react-router";
 import { z } from "zod";
-
-import type { LoginState } from "@/store/login";
-
-import { useLoginStore } from "@/store/login";
-import { useMySelfStore } from "@/store/user";
 
 const sessionSearchSchema = z.object({
   session: z.string().uuid(),
@@ -13,49 +7,4 @@ const sessionSearchSchema = z.object({
 
 export const Route = createFileRoute("/login/_layout/getToken")({
   validateSearch: sessionSearchSchema,
-  component: GetToken,
 });
-
-function GetToken() {
-  const { session } = Route.useSearch();
-  const navigate = useNavigate();
-  const login = useLoginStore();
-
-  const tokenUrl = `https://${login.instance}/api/miauth/${session}/check`;
-  fetchData(tokenUrl, login);
-
-  useEffect(() => {
-    if (login.isLogin) {
-      navigate({ to: "/", replace: true });
-    }
-  }, [login, navigate]);
-
-  return <>loading...</>;
-}
-
-const fetchData = async (tokenUrl: string, login: LoginState) => {
-  const setMyself = useMySelfStore.setState;
-  const setLogin = useLoginStore.setState;
-
-  const data = await fetch(tokenUrl, {
-    method: "POST",
-  })
-    .then((res) => {
-      if (!res.ok) {
-        throw new Error(`${res.status} ${res.statusText}`);
-      }
-      return res.json();
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-
-  if (data.token) {
-    setMyself({ mySelf: data.user });
-    setLogin({
-      ...login,
-      isLogin: true,
-      token: data.token,
-    });
-  }
-};
